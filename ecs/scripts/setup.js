af801ffa374902407b53089851a1463c1beeea84
Cesar Laredo <cesar.f.laredo@gmail.com>
2018-06-03 18:48:45 -0700

    Update region, config AWS credentials

--- ecs/scripts/setup.js
+++ ecs/scripts/setup.js
@@ -7,7 +7,7 @@ const AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID;
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 const AWS_USERNAME = process.env.AWS_USERNAME;
-const AWS_CONFIG_REGION = 'us-west-2';
+const AWS_CONFIG_REGION = 'us-east-2';
 
 const clusterName = 'microservicemovies-review';
 
